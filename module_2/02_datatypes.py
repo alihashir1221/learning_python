827a873d7b63b31c812bd69dafbb9a61bc8e1cfc
Ali Hashir <alihashir1221@gmail.com>
2026-08-25 23:17:46 +0530

    learning_datatype

---
+++ module_2/02_datatypes.py
@@ -0,0 +1,44 @@
+#TEXT TYPE
+#str
+str="Hello World"
+
+#NUMERIC TYPES
+#int
+int=5
+#float
+float=5.5
+#complex
+complex=1+2j
+
+#SEQUENCE TYPES
+#list
+list = ["apple", "banana", "cherry"]
+#tuple
+tuple = ("apple", "banana", "cherry")   
+#range
+range = range(6)
+
+#MAPPING TYPE
+#dict
+dict = {"name" : "John", "age" : 36}
+
+#SET TYPES
+#set
+set = {"apple", "banana", "cherry"}
+#frozenset
+frozenset = frozenset({"apple", "banana", "cherry"})
+
+#BOOLEAN TYPE
+#bool
+bool = True
+
+#BINARY TYPES
+#bytes
+bytes = b"Hello"
+#bytearray
+bytearray = bytearray(5)
+#memoryview
+memoryview = memoryview(bytes(5))
+#NONE TYPE
+#None
+none = None
