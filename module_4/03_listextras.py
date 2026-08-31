0a9fcfcfc36d991faa86b212a96bdd378e24bbad
Ali Hashir <alihashir1221@gmail.com>
2026-08-31 22:18:31 +0530

    extra functions in list

---
+++ module_4/03_listextras.py
@@ -0,0 +1,12 @@
+#this prints the length of list len()
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#prints the type of the list .... type()
+mylist = [7, "banana", "cherry"]
+print(type(mylist))
+
+#list constructor is used to create a list ...lis()
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+ 
