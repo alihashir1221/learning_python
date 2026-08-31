a200aa1bc76e41fba501ee95fe86f845a720d563
Ali Hashir <alihashir1221@gmail.com>
2026-08-31 22:11:44 +0530

    learnt about list methods

--- module_4/01_list.py
+++ module_4/01_list.py
@@ -6,3 +6,7 @@ print(list)
 print(list[0]) #accessing the first element of the list
 print(list[3]) #accessing the second element of the list
 print(list[2]) #accessing the third element of the list
+
+#Unlike strings lists are mutable, meaning you can change their content after creation. You can add, remove, or modify elements in a list.
+list[0] = "Grapes" #changing the first element of the list
+print(list)
---
+++ module_4/02_listmethods.py
@@ -0,0 +1,25 @@
+variable = [1,45,2,98,4,7,4,1,5,6,89,99,785]
+
+#sorts the list in ascending order
+variable.sort()
+print(variable)
+
+#list.reverse() reverses the list from end to start
+variable.reverse()
+print(variable)
+
+#list.append(add item in the list)
+variable.append(784)
+print(variable)
+
+#list.insert(this is the index, this the item that needs to be added)
+variable.insert(2,85)
+print(variable)
+
+#list.pop(delete the item at that index)
+variable.pop(5)
+print(variable)
+
+#list.remove(removes the item from the list)
+variable.remove(99)
+print(variable)
