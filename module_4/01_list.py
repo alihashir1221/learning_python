a1f7ee6df04f27e73727c7b4c48cc2390e408aee
Ali Hashir <alihashir1221@gmail.com>
2026-08-31 21:30:23 +0530

    started learning list

---
+++ module_4/01_list.py
@@ -0,0 +1,8 @@
+#LISTS AND TUPLES 
+#IN Python, lists and tuples are both used to store collections of items. However, there are some key differences between the two.
+#Lists are mutable, meaning you can change their content after creation.
+list = ["ali", 7 , True, 3.14]
+print(list)
+print(list[0]) #accessing the first element of the list
+print(list[3]) #accessing the second element of the list
+print(list[2]) #accessing the third element of the list
