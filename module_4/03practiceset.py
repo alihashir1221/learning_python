f4e515f868487e84bd86813a7110e50fb75caaab
Ali Hashir <alihashir1221@gmail.com>
2026-09-02 21:09:47 +0530

    practice set 3

---
+++ module_4/03practiceset.py
@@ -0,0 +1,16 @@
+#WAP to sum a list within 4 numbers 
+num = []
+a = int(input("Enter 1st number: "))
+num.append(a)
+b = int(input("Enter 2nd number: "))
+num.append(b)
+c = int(input("Enter 3rd number: "))
+num.append(c)
+d = int(input("Enter 4th number: "))
+num.append(d)
+print(sum(num))
+
+#using user input sum
+
+list = [1, 2, 3, 4]
+print(sum(list))
