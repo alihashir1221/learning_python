4b4f033b80178c718634322ac7fa88d6e00bb7ce
Ali Hashir <alihashir1221@gmail.com>
2026-09-02 20:58:30 +0530

    practice set 2

---
+++ module_4/2practiceset.py
@@ -0,0 +1,18 @@
+#wap to accept marsk of six studnets and sort them 
+marks = []
+student1 = input("Marks of 1 student")
+marks.append(student1)
+student2 = input("Marks of 2 student")
+marks.append(student2)
+student3 = input("Marks of 3 student")
+marks.append(student3)
+student4 = input("Marks of 4 student")
+marks.append(student4)
+student5 = input("Marks of 5 student")
+marks.append(student5)
+student6 = input("Marks of 6 student")
+marks.append(student6)
+marks.sort() #sorts the list in ascending order
+print(marks)
+
+
