d68b41b4dfec2586a72ca99f06f52224805358b2
Ali Hashir <alihashir1221@gmail.com>
2026-09-03 22:29:24 +0530

    more about dictionaries

--- module_5/02dict_methods.py
+++ module_5/02dict_methods.py
@@ -7,3 +7,9 @@ marks = {
 }
 
 print(marks.items()) #returns a list of tuples containing key value pairs
+print(marks.keys()) #returns a list of keys
+marks.update({"Ali": 90}) #updates the value of the key
+print(marks) #prints the updated dictionary
+print(marks.values()) #returns a list of values
+print(marks.get("Ali")) #returns the value of the key
+print(marks["Adnan"]) #returns the value of the key if key is not found returns the default value
