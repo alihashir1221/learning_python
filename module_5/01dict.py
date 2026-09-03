c4d90cf07059ddc78cb692bff12f1afbeea79349
Ali Hashir <alihashir1221@gmail.com>
2026-09-03 21:11:03 +0530

    started learning dictionary

---
+++ module_5/01dict.py
@@ -0,0 +1,9 @@
+#Dictionary is a collection of key value pairs. It is unordered, mutable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.
+marks = {
+    "Ali": 87,
+    "Tushar": 89,
+    "Adnan": 90,
+    list: [1,2,3,4]
+}
+#print(marks, type(marks))
+print(marks[list]) #accessing value using key
