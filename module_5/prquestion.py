7da7a4a5ddd3cabfe3e5e6d65718032c7aa77e82
Ali Hashir <alihashir1221@gmail.com>
2026-09-04 21:31:51 +0530

    few more questions

---
+++ module_5/prquestion.py
@@ -0,0 +1,19 @@
+# 1. len() - finds the length
+name = "Ali"
+print("Length:", len(name))
+
+# 2. type() - tells the data type
+x = 10
+print("Type:", type(x))
+
+# 3. sum() - adds all numbers
+marks = [80, 90, 85]
+print("Sum:", sum(marks))
+
+# 4. max() - finds the largest number
+numbers = [10, 25, 15]
+print("Maximum:", max(numbers))
+
+# 5. min() - finds the smallest number
+numbers = [10, 25, 15]
+print("Minimum:", min(numbers))
