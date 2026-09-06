437ee23ca81633d5f8bb763e8e896ac8a38a63a3
Ali Hashir <alihashir1221@gmail.com>
2026-09-06 22:23:28 +0530

    done

--- module_6/02if_elif_else_lader.py
+++ module_6/02if_elif_else_lader.py
@@ -1,11 +1,17 @@
-#IF ELSE STATEMENT
-a = int(input("Enter your age: "))
-if(a > 18):
-    print("Adult")
-elif(a<0):
-    print("Invalid negative age entered")
-elif(a==0):
-    print("You are entering an invalid age")
+# #IF ELSE STATEMENT
+# a = int(input("Enter your age: "))
+# if(a > 18):
+#     print("Adult")
+# elif(a<0):
+#     print("Invalid negative age entered")
+# elif(a==0):
+#     print("You are entering an invalid age")
+# else:
+#     print("Minor")
+# print("End of program.")
+a = input("Enter name: ")
+b = input("Enter second name: ")
+if(a == "Ali") or (b == "Hashir"):
+    print("ok")
 else:
-    print("Minor")
-print("End of program.")
+    print("not ok")
