f62f4698910e805fba6038efd1bea9164fb1da3f
Ali Hashir <alihashir1221@gmail.com>
2026-09-09 01:15:23 +0530

    solved question 5 and 6

---
+++ module_7/06pr_questions_forloop.py
@@ -0,0 +1,3 @@
+#to print star from bottom to top
+for i in range(7, 0, -1):
+    print("*" * i) 
---
+++ module_7/5pr_questions_forloop.py
@@ -0,0 +1,6 @@
+#sum of 1-50
+total = 0
+for i in range(1, 51):
+    total += i
+    print(total)
+print(total)
