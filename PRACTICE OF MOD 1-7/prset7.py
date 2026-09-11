6c141e9bbb9287db57a76939128c14bd41a27f21
Ali Hashir <alihashir1221@gmail.com>
2026-09-12 02:04:23 +0530

    List + for loop

---
+++ PRACTICE OF MOD 1-7/prset7.py
@@ -0,0 +1,16 @@
+'''
+List + for loop
+
+Given:
+numbers = [12, 5, 8, 21, 10, 7, 16]
+Use a for loop to print only the numbers greater than 10.
+
+Expected:
+12
+21
+16
+'''
+numbers = [12, 5, 8, 21, 10, 7, 16]
+for number in numbers:
+    if number >= 10:
+        print(number)
