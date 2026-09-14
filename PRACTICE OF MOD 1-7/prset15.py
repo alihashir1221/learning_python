ff8d7e26688a4b657a6630402c4d1fe0a1f88c42
Ali Hashir <alihashir1221@gmail.com>
2026-09-14 23:14:37 +0530

    differenve between set list and tuples

---
+++ PRACTICE OF MOD 1-7/prset15.py
@@ -0,0 +1,7 @@
+'''
+What is the difference between a list, tuple, and set in Python?
+
+list: A list is an ordered collection of items that can be changed (mutable). Lists are defined using square brackets [] and can contain duplicate elements.
+tuple: A tuple is an ordered collection of items that cannot be changed (immutable). Tuples are defined using parentheses () and can also contain duplicate elements.
+set: A set is an unordered collection of unique items that can be changed (mutable). Sets are defined using curly braces {} or the set() function.
+'''
