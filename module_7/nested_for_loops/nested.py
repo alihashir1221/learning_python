f1aa95593c9f9cfe7c579ef656dbbefd89a1b5a0
Ali Hashir <alihashir1221@gmail.com>
2026-09-19 22:41:31 +0530

    more about nested loops

---
+++ module_7/nested_for_loops/nested.py
@@ -0,0 +1,51 @@
+for x in range(3):  #outer loop
+    for y in range(6):    #inner loop
+        for z in range(2):      #inner loop
+            print(f"x: {x} and y: {y} and z: {z}")
+
+
+
+
+
+
+
+
+'''
+output:
+ x: 0 and y: 0 and z: 0
+x: 0 and y: 0 and z: 1
+x: 0 and y: 1 and z: 0
+x: 0 and y: 1 and z: 1
+x: 0 and y: 2 and z: 0
+x: 0 and y: 2 and z: 1
+x: 0 and y: 3 and z: 0
+x: 0 and y: 3 and z: 1
+x: 0 and y: 4 and z: 0
+x: 0 and y: 4 and z: 1
+x: 0 and y: 5 and z: 0
+x: 0 and y: 5 and z: 1
+x: 1 and y: 0 and z: 0
+x: 1 and y: 0 and z: 1
+x: 1 and y: 1 and z: 0
+x: 1 and y: 1 and z: 1
+x: 1 and y: 2 and z: 0
+x: 1 and y: 2 and z: 1
+x: 1 and y: 3 and z: 0
+x: 1 and y: 3 and z: 1
+x: 1 and y: 4 and z: 0
+x: 1 and y: 4 and z: 1
+x: 1 and y: 5 and z: 0
+x: 1 and y: 5 and z: 1
+x: 2 and y: 0 and z: 0
+x: 2 and y: 0 and z: 1
+x: 2 and y: 1 and z: 0
+x: 2 and y: 1 and z: 1
+x: 2 and y: 2 and z: 0
+x: 2 and y: 2 and z: 1
+x: 2 and y: 3 and z: 0
+x: 2 and y: 3 and z: 1
+x: 2 and y: 4 and z: 0
+x: 2 and y: 4 and z: 1
+x: 2 and y: 5 and z: 0
+x: 2 and y: 5 and z: 1
+'''
