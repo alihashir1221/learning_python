ecacdeca614b720583da5638f2a5f4222e19aa33
Ali Hashir <alihashir1221@gmail.com>
2026-09-22 21:57:03 +0530

    function with arguments

---
+++ module_8/functions/func_with_argument.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+    print("Good Day! " + name)
+    print(ending)
+
+goodDay("Ali", "Thank You!")
+goodDay("Ajith", "Thank You!")
+goodDay("Atash", "Thank You!")
+goodDay("Akash", "Thank You!")
