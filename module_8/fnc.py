8935255222b29889303aa4d404e64613c919860a
Ali Hashir <alihashir1221@gmail.com>
2026-09-22 21:18:38 +0530

    started learning functions

---
+++ module_8/fnc.py
@@ -0,0 +1,15 @@
+def avg():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    c = int(input("Enter one more number: "))
+    average = (a + b + c) / 3
+    print("The average of the three numbers is:", average)
+
+avg()
+avg()
+avg()
+avg()
+avg()
+avg()
+avg()
+
