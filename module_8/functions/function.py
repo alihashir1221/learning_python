7565c2ceaa0bbf1198991797ee5cfc1faba3fade
Ali Hashir <alihashir1221@gmail.com>
2026-09-23 22:08:36 +0530

    learning function from code with baraa

---
+++ module_8/functions/function.py
@@ -0,0 +1,12 @@
+def make_coffee():
+    print("Start Machine")
+    print("Make Coffe")
+    print("Add Milk")
+    print("Enjoy it")
+
+make_coffee()
+print("Wake up")
+print("Working ")
+make_coffee()
+make_coffee()
+make_coffee()
