1dc16a45839dffe22578717f0f99a8dbbe00697b
Ali Hashir <alihashir1221@gmail.com>
2026-09-23 22:12:04 +0530

    code with baraa built in functions

---
+++ module_8/functions/builtinfunctions.py
@@ -0,0 +1,12 @@
+import math
+#Built in function (Just Calling)
+print(len("Python"))
+
+#Function from libraries
+number = 4.2
+print(math.ceil(number))
+
+#User defined function
+def greet():
+    print("Hello")
+greet()
