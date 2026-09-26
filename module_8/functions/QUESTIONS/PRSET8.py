605f7ca1675e9e2e245368b882c803cba69632d2
Ali Hashir <alihashir1221@gmail.com>
2026-09-26 21:03:29 +0530

    Practice set 3

---
+++ module_8/functions/QUESTIONS/PRSET8.py
@@ -0,0 +1,11 @@
+'''
+Q3. Write a function details(name, course, year) that prints:
+Name: Ali
+Course: BCA
+Year: 1
+Call the function using positional arguments.
+'''
+def details(name, course, year):
+    print(f"Name: {name}, \nCourse: {course}, \nYear: ", year)
+
+details("Ali", "MCA", "1")
