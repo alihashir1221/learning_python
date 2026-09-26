f91ed3f75ebb2ff7e8146e5042bedeffde67113d
Ali Hashir <alihashir1221@gmail.com>
2026-09-26 20:57:13 +0530

    practice set 2

---
+++ module_8/functions/QUESTIONS/PRSET7.py
@@ -0,0 +1,8 @@
+'''
+Postional Arguments
+Q2. Write a function multiply(a, b, c) that takes three positional arguments and prints their product. Call it with 5, 4, 2.
+'''
+def multiply(a, b, c):
+    print(a*b*c)
+
+multiply(5, 4, 2)
